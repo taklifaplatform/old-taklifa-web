7b18e309e448ebeb332f3895daec506477d5df82
Chawki Ifaoui <chawki@zixdev.com>
2024-11-19 10:29:00 +0100

    Remove postcode requirement in CartAddressRequest validation

--- packages/Webkul/Shop/src/Http/Requests/CartAddressRequest.php
+++ packages/Webkul/Shop/src/Http/Requests/CartAddressRequest.php
@@ -54,7 +54,7 @@ class CartAddressRequest extends FormRequest
             "{$addressType}.city"         => ['required'],
             "{$addressType}.country"    => [new AlphaNumericSpace],
             "{$addressType}.state"      => [new AlphaNumericSpace],
-            "{$addressType}.postcode"     => core()->isPostCodeRequired() ? ['required', 'numeric'] : ['numeric'],
+            "{$addressType}.postcode"   => ['numeric'],
             "{$addressType}.phone"        => ['required', new PhoneNumber],
         ]);
     }
